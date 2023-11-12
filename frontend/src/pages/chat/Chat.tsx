afc3c1a1de3abc732537802e3d60a78439c2aee9
Garducci <brunogarducci@gmail.com>
2023-11-12 15:09:11 +1100

    Update Chat.tsx

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -571,7 +571,7 @@ const Chat = () => {
                         {!messages || messages.length < 1 ? (
                             <Stack className={styles.chatEmptyState}>
                                 <img
-                                    src={Azure}
+                                    src={norteenergia-black}
                                     className={styles.chatIcon}
                                     aria-hidden="true"
                                 />
